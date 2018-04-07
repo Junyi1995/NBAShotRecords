84b079458f82f9ff62decd8cdcf115d50e259db6
Junyi1995 <jh2635@cornell.edu>
2018-04-07 18:16:02 -0400

    add styles and set header

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import nba from 'nba';
+//import {ShortChart} from './ShotChart';
+
+
+export class Main extends React.Component{
+    render() {
+        return (
+            <div className = "main">
+            " Hi,  this is main!"
+            </div>
+        );
+    }
+}
