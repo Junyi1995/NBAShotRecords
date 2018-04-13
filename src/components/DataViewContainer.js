594a5080275935d79fede69a7cf24e22cba3f958
Junyi1995 <jh2635@cornell.edu>
2018-04-12 22:28:28 -0400

    add DataViewContainer

---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ShotChart} from "./ShotChart";
+
+export class DataViewContainer extends React.Component{
+    render() {
+        return(
+            <div className = 'data-view'>
+                <ShotChart playerId={this.props.playerId}/>
+                </div>
+        );
+    }
+
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import nba from 'nba';
-import {ShotChart} from './ShotChart';
 import {Profile} from './Profile';
+import {DataViewContainer} from "./DataViewContainer"
 
 export class Main extends React.Component{
     state = {
@@ -18,8 +18,9 @@ export class Main extends React.Component{
     render() {
         return (
             <div className = "main">
-            <Profile/>
-             <ShotChart playerId = {this.state.playerId}/>
+            <Profile playerId = {this.state.playerId}
+                playerInfo = {this.state.playerInfo}/>
+                <DataViewContainer playerId = {this.state.playerId}/>
             </div>
         );
     }
