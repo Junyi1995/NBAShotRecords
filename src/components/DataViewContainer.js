df2d653cf45b422b5bac79ad5247f355651c5537
Junyi1995 <jh2635@cornell.edu>
2018-04-12 23:04:18 -0400

    add changeValue

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {ShotChart} from "./ShotChart";
+import {CountSlider} from './CountSlider';
 
 export class DataViewContainer extends React.Component{
+    state = {
+        minCount : 2,
+    }
+    onCountSliderChange = (count) => {
+        this.setState({ minCount:count });
+    }
     render() {
         return(
             <div className = 'data-view'>
-                <ShotChart playerId={this.props.playerId}/>
+                <ShotChart playerId={this.props.playerId} minCount = {this.state.minCount}/>
+                <CountSlider onCountSliderChange = {this.onCountSliderChange}/>
                 </div>
         );
     }
